1467d3755fe6ca2398e915e3f8bc3521f206ed37
Abdulkadir Polat <tr.abdulkadir.polat@gmail.com>
2021-09-08 22:08:28 +0300

    Initial commit

---
+++ src/Components/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ListImg from "./ListImg";
+import SearchBar from "./SearchBar";
+
+function App() {
+  return (
+    <div className="container mx-auto bg-gray-200 min-h-screen ">
+      <SearchBar />
+      <ListImg />
+    </div>
+  );
+}
+
+export default App;
